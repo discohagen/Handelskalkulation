6d941d87d05922958de6b32f1024ce38cf72658e
Discohagen <leonziegenhagen22@gmail.com>
2024-05-04 18:59:09 +0200

    feat: finished rueckwaertskalkulation

--- script.js
+++ script.js
@@ -145,8 +145,70 @@ function loadForwardCalculation() {
     ]);
 }
 
-// ToDo
-function loadBackwardCalculation() {}
+function loadBackwardCalculation() {
+    const backwardCalculationTable =
+        document.getElementById(
+            'rueckwaertskalkulationstabelle'
+        );
+
+    for (let i = 0; i < calculationSteps.length; i++) {
+        let row = backwardCalculationTable.insertRow();
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+
+        cell1.innerHTML = signsForward[i];
+        cell2.innerHTML = calculationSteps[i];
+
+        let input1 = document.createElement('input');
+        input1.type = 'number';
+        input1.min = 0;
+        input1.max = 100;
+        input1.id = `bw${calculationSteps[i].replace(
+            /\s/g,
+            ''
+        )}Percentage`;
+        cell3.appendChild(input1);
+
+        let input2 = document.createElement('input');
+        input2.type = 'number';
+        input2.min = 0;
+        input2.id = `bw${calculationSteps[i].replace(
+            /\s/g,
+            ''
+        )}Amount`;
+        cell4.appendChild(input2);
+    }
+
+    disableElements([
+        'bwListeneinkaufspreis(netto)Percentage',
+        'bwListeneinkaufspreis(netto)Amount',
+        'bwLieferrabattAmount',
+        'bwZieleinkaufspreisPercentage',
+        'bwZieleinkaufspreisAmount',
+        'bwLieferskontoAmount',
+        'bwBareinkaufspreisPercentage',
+        'bwBareinkaufspreisAmount',
+        'bwBezugskostenPercentage',
+        'bwBezugspreisPercentage',
+        'bwBezugspreisAmount',
+        'bwHandlungskostenzuschlagAmount',
+        'bwSelbstkostenPercentage',
+        'bwSelbstkostenAmount',
+        'bwGewinnzuschlagAmount',
+        'bwBarverkaufspreisPercentage',
+        'bwBarverkaufspreisAmount',
+        'bwKundenskontoAmount',
+        'bwZielverkaufspreisPercentage',
+        'bwZielverkaufspreisAmount',
+        'bwKundenrabattAmount',
+        'bwListenverkaufspreis(netto)Percentage',
+        'bwListenverkaufspreis(netto)Amount',
+        'bwMehrwertsteuerAmount',
+        'bwListenverkaufspreis(brutto)Percentage',
+    ]);
+}
 
 function forwardCalculation() {
     document.getElementById('LieferrabattAmount').value =
@@ -247,8 +309,109 @@ function forwardCalculation() {
     );
 }
 
-// ToDo
-function backwardCalculation() {}
+function backwardCalculation() {
+    document.getElementById(
+        'bwMehrwertsteuerAmount'
+    ).value =
+        calculatePercentageOfAmountCustomerPerspectiveFromBackwards(
+            'bwListenverkaufspreis(brutto)Amount',
+            'bwMehrwertsteuerPercentage'
+        );
+
+    document.getElementById(
+        'bwListenverkaufspreis(netto)Amount'
+    ).value = subtractValues(
+        'bwListenverkaufspreis(brutto)Amount',
+        'bwMehrwertsteuerAmount'
+    );
+
+    document.getElementById('bwKundenrabattAmount').value =
+        calculatePercentageOfAmount(
+            'bwListenverkaufspreis(netto)Amount',
+            'bwKundenrabattPercentage'
+        );
+
+    document.getElementById(
+        'bwZielverkaufspreisAmount'
+    ).value = subtractValues(
+        'bwListenverkaufspreis(netto)Amount',
+        'bwKundenrabattAmount'
+    );
+
+    document.getElementById('bwKundenskontoAmount').value =
+        calculatePercentageOfAmount(
+            'bwZielverkaufspreisAmount',
+            'bwKundenskontoPercentage'
+        );
+
+    document.getElementById(
+        'bwBarverkaufspreisAmount'
+    ).value = subtractValues(
+        'bwZielverkaufspreisAmount',
+        'bwKundenskontoAmount'
+    );
+
+    document.getElementById(
+        'bwGewinnzuschlagAmount'
+    ).value =
+        calculatePercentageOfAmountCustomerPerspectiveFromBackwards(
+            'bwBarverkaufspreisAmount',
+            'bwGewinnzuschlagPercentage'
+        );
+
+    document.getElementById('bwSelbstkostenAmount').value =
+        subtractValues(
+            'bwBarverkaufspreisAmount',
+            'bwGewinnzuschlagAmount'
+        );
+
+    document.getElementById(
+        'bwHandlungskostenzuschlagAmount'
+    ).value =
+        calculatePercentageOfAmountCustomerPerspectiveFromBackwards(
+            'bwSelbstkostenAmount',
+            'bwHandlungskostenzuschlagPercentage'
+        );
+
+    document.getElementById('bwBezugspreisAmount').value =
+        subtractValues(
+            'bwSelbstkostenAmount',
+            'bwHandlungskostenzuschlagAmount'
+        );
+
+    document.getElementById(
+        'bwBareinkaufspreisAmount'
+    ).value = subtractValues(
+        'bwBezugspreisAmount',
+        'bwBezugskostenAmount'
+    );
+
+    document.getElementById('bwLieferskontoAmount').value =
+        calculatePercentageOfAmountFromBackwards(
+            'bwBareinkaufspreisAmount',
+            'bwLieferskontoPercentage'
+        );
+
+    document.getElementById(
+        'bwZieleinkaufspreisAmount'
+    ).value = addValues(
+        'bwBareinkaufspreisAmount',
+        'bwLieferskontoAmount'
+    );
+
+    document.getElementById('bwLieferrabattAmount').value =
+        calculatePercentageOfAmountFromBackwards(
+            'bwZieleinkaufspreisAmount',
+            'bwLieferrabattPercentage'
+        );
+
+    document.getElementById(
+        'bwListeneinkaufspreis(netto)Amount'
+    ).value = addValues(
+        'bwZieleinkaufspreisAmount',
+        'bwLieferrabattAmount'
+    );
+}
 
 function disableElements(elementIds) {
     elementIds.forEach((elementId) => {
@@ -319,6 +482,49 @@ function calculatePercentageOfAmountCustomerPerspective(
                     document.getElementById(
                         percentageElementId
                     ).value)
+        ) / 100
+    );
+}
+
+function calculatePercentageOfAmountCustomerPerspectiveFromBackwards(
+    amountElementId,
+    percentageElementId
+) {
+    return (
+        Math.round(
+            (100 *
+                (document.getElementById(amountElementId)
+                    .value *
+                    document.getElementById(
+                        percentageElementId
+                    ).value)) /
+                Number(
+                    Number(
+                        document.getElementById(
+                            percentageElementId
+                        ).value
+                    ) + 100
+                )
+        ) / 100
+    );
+}
+
+function calculatePercentageOfAmountFromBackwards(
+    amountElementId,
+    percentageElementId
+) {
+    return (
+        Math.round(
+            100 *
+                ((document.getElementById(amountElementId)
+                    .value *
+                    document.getElementById(
+                        percentageElementId
+                    ).value) /
+                    (100 -
+                        document.getElementById(
+                            percentageElementId
+                        ).value))
         ) / 100
     );
 }
